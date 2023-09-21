62a0785a306187775d39926154609229d5f97df7
walidemad <walidemad313@gmail.com>
2023-09-21 12:14:51 +0300

    Sorry for the late reply

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-function countEmojis(str) {}
+function countEmojis(inputString) {
+    const emojiRegex = /[\p{Emoji}]/gu;
+    const emojiCounts = {};
+    const emojis = inputString.match(emojiRegex);
+
+    if (!emojis) {
+        return emojiCounts;
+    }
+    emojis.forEach(emoji => {
+        emojiCounts[emoji] = (emojiCounts[emoji] || 0) + 1
+    })
+    return emojiCounts
+}
 
 // Test Function:
-const str = "Hello, I ❤️ JavaScript! 🚀🚀🚀";
+const str = "Hello, I ❤️ JavaScript! 🚀🚀🚀 😊🙂 😊🙂 ";
 console.log(countEmojis(str));
